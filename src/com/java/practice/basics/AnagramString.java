d5c783dcc4d553dc570d4fe22b5ec8b44249e585
Avinash Walke <avinashw05@gmail.com>
2025-05-02 00:06:26 +0530

    optimised anagram string code

--- src/com/java/practice/basics/AnagramString.java
+++ src/com/java/practice/basics/AnagramString.java
@@ -1,18 +1,37 @@
 package com.java.practice.basics;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class AnagramString {
 
-    private static boolean isAnagram(String str1, String str2) {
-
+    private static boolean isAnagramUsingOptimisedWayHashMap(String str1, String str2) {
         if (str1.length() != str2.length()) {
             return false;
         }
 
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
-        HashMap<Character, Integer> map = new HashMap<>();
+
+        Map<Character, Integer> anagramMap = new HashMap<>();
+        for (int i = 0; i < str1.length(); i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+
+            anagramMap.put(c1, anagramMap.getOrDefault(c1, 0) + 1);
+            anagramMap.put(c2, anagramMap.getOrDefault(c2, 0) - 1);
+        }
+        return anagramMap.containsValue(0);
+    }
+
+    private static boolean isAnagramUsingHashMap(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        Map<Character, Integer> map = new HashMap<>();
 
         for (char c : str1.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
@@ -31,12 +50,18 @@ public class AnagramString {
     }
 
     public static void main(String[] args) {
-        String s1 = "Keep"; // listen
-        String s2 = "Peek"; // silent
-        if (isAnagram(s1, s2)) {
-            System.out.println(s1 + " and " + s2 + " are anagrams");
+        String s1 = "keep"; // listen
+        String s2 = "peek"; // silent
+        if (isAnagramUsingHashMap(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams using hashMap approach");
         } else {
-            System.out.println(s1 + " and " + s2 + " are not anagrams");
+            System.out.println(s1 + " and " + s2 + " are not anagrams using hashMap approach");
+        }
+
+        if (isAnagramUsingOptimisedWayHashMap(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams using optimised way hashMap approach");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams using optimised way hashMap approach");
         }
     }
 }
